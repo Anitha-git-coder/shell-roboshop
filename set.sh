cde34ebe11c13400ff79fc600f74fa9ad00df835
Anitha <anitha.pinipe14@gmail.com>
2025-10-29 14:13:23 +0530

    variables 24-10-25

--- set.sh
+++ set.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#!/bin/bash
 set -e
-error(){
-    echo "error in line no:$LINENO,cmd failed: $BASH_COMMAND"
-}
 
-trap 'error' ERR
+# Track the last executed command and its line number
+trap 'last_command=$current_command; current_command=$BASH_COMMAND; last_lineno=$LINENO' DEBUG
+
+# Report error using the last tracked command and line number
+trap 'echo "Error on line $last_lineno: $last_command"' ERR
 
 echo "hello"
 echo "before error"
-s;aslas  # singl ERR
-echo "after error"
+s;aslas  # This will trigger the trap
+echo "after error"  # Won't run due to set -e
