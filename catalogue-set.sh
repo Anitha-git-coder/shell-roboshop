5369959dd721afc3681922efb6b55d6863b2a4f1
Anitha <anitha.pinipe14@gmail.com>
2025-10-29 14:25:25 +0530

    variables 24-10-25

---
+++ catalogue-set.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -euo pipefail
+trap 'echo "error in line no:$LINENO,cmd failed: $BASH_COMMAND"' ERR
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.anitha.fun
+#echo "13-logs.sh" | cut -d "." -f1
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+USERID=$(id -u)
+
+if [ $USERID  -ne 0 ]; then
+    echo -e "error :: run with root privelege"
+    exit 1
+fi
+mkdir -p $LOGS_FOLDER
+echo "script started at $(date)" | tee -a $LOG_FILE
+#------NODEJS------#
+dnf module disable nodejs -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf install nodejs -y &>>$LOG_FILE
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+else
+    echo -e "user already exits.... $Y skipping $N"
+fi
+mkdir -p /app 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+cd /app 
+rm -rf /app/*
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+npm install &>>$LOG_FILE
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+dnf install mongodb-mongosh kkkk -y &>>$LOG_FILE
+INDEX=$(mongosh mongodb.anitha.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ INDEX -ne 0]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE   
+else 
+    echo -e "already loaded $Y skipping $N"
+fi
+systemctl restart catalogue
+
+
+
+
+
