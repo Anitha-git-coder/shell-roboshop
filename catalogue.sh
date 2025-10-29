18d3ac77c85f18da653175a5055a345989af5f17
Anitha <anitha.pinipe14@gmail.com>
2025-10-29 12:56:51 +0530

    variables 24-10-25

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "creating system user"
 else
-    echo "user already exits.... $Y skipping $N
+    echo "user already exits.... $Y skipping $N"
 fi
 mkdir -p /app 
 VALIDATE $? "creating app directory"
