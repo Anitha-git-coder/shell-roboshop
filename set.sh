c05af8a932b82963b7e1784f4036d4c9349fbef9
Anitha <anitha.pinipe14@gmail.com>
2025-10-29 14:09:23 +0530

    variables 24-10-25

--- set.sh
+++ set.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 set -e
+error()
+{
+    echo "error in line no:$LINENO,cmd failed: $BASH_COMMAND"
+}
+
+trap error ERR
+
 echo "hello"
 echo "before error"
-s;aslas
+s;aslas  # singl ERR
 echo "after error"
