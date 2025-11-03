c54188e21fe1b39977f2bb1736b182e369fbd95d
Anitha <anitha.pinipe14@gmail.com>
2025-11-03 10:08:43 +0530

    msg

---
+++ script-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME=India
+echo "my country is $NAME"
+echo "process id script-1: $$"
+sh script-2.sh
---
+++ script-2.sh
@@ -0,0 +1,4 @@
+#!/bib/bash
+
+echo "my country is : $NAME"
+echo "PID of script-2 is : $$"
